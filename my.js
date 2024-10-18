9271cd3f638ea8a7f14fdf03580c0595ea9c712b
bayas689 <axotaim49@gmail.com>
2024-10-18 10:08:46 +0600

    Add files via upload

---
+++ my.js
@@ -0,0 +1,23 @@
+
+let res=prompt("?")
+    switch (res) {
+        case "31":
+        alert("бир аз ойлонуп корунуз")
+        break;
+
+        case '32':
+        alert('туура')
+        break;
+
+        case '33':
+        alert('туура эмес')
+        break;
+
+        default: 
+        alert('дагы ойлонунуз')
+        break;
+
+    }
+
+
+
